8ae0e06ba149e2ee4ae5ad7c563640235c0269cf
Shubham Kumar Singh <37653026+denyshubh@users.noreply.github.com>
2019-09-17 19:32:05 +0530

    changed text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HELLO WORLD!
+          Hello Guys How are You!!
         </p>
         <a
           className="App-link"
